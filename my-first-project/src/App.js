0b7689fa2d8cc7c5f865740bd0fe2f6b3a002275
Angelurbina4 <angelurbina44@gmail.com>
2022-04-29 14:23:34 -0400

    Primer React JSX Core

---
+++ my-first-project/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {    
+  render() {        
+      return (       
+        <div>
+          <h1>Hello Dojo</h1> 
+          <h3>Things i need to do:</h3> 
+          <ul>
+            <li>Learn React</li>
+            <li>Learn React</li>
+            <li>Learn React</li>
+            <li>Learn React</li>
+          </ul> 
+        </div>
+      );    
+  }
+}
+
+export default App;
